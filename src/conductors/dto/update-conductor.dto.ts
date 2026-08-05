6d35ae2f4c5b4dfdaffdd4de9bf7f11321a74a2f
MRamadhan000 <mcahya830@gmail.com>
2026-08-06 00:57:07 +0700

    feat : update condectur dto

--- src/conductors/dto/update-conductor.dto.ts
+++ src/conductors/dto/update-conductor.dto.ts
@@ -1,4 +1,14 @@
+import { IsBoolean, IsEnum, IsOptional } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateConductorDto } from './create-conductor.dto';
+import { ConductorStatus } from '../entities/conductor.entity';
 
-export class UpdateConductorDto extends PartialType(CreateConductorDto) {}
+export class UpdateConductorDto extends PartialType(CreateConductorDto) {
+  @IsBoolean({ message: 'Status verifikasi harus berupa boolean.' })
+  @IsOptional()
+  isVerified?: boolean;
+
+  @IsEnum(ConductorStatus, { message: 'Status kondektur tidak valid.' })
+  @IsOptional()
+  status?: ConductorStatus;
+}
